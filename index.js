18886d944af1756b93a56839de1d02117c336c35
Tymur Bondar <tymurbondar@outlook.com>
2023-09-01 15:09:44 -0400

    manipulating DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const el = document.getElementById("main");
+el.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+newHeader.textContent = 'Tymur Bondar is the champion';
+
+document.body.appendChild(newHeader);
